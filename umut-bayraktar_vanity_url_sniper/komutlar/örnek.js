3f47faeb604b198c07330887e8cf304d3766f461
yunusbeyy <77726399+yunusbeyy@users.noreply.github.com>
2022-01-02 15:43:17 +0300

    Add files via upload

---
+++ umut-bayraktar_vanity_url_sniper/komutlar/örnek.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+message.channel.send("https://discord.gg/6XGqdgE")
+};
+exports.conf = {
+  aliases: []
+};
+
+exports.help = {
+  name: "link"
+};
